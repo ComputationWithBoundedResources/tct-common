87d5c6713a9e6de9216514d799122681b2415580
mschaper <michael.schaper@uibk.ac.at>
2014-12-09 17:36:49 +0100

    fixed: YES(?,?) is an illegal answer

--- src/Tct/Common/Answer.hs
+++ src/Tct/Common/Answer.hs
@@ -3,23 +3,24 @@ module Tct.Common.Answer
   , answering
   ) where
 
-import Tct.Core.Data hiding (Unknown)
+import qualified Tct.Core.Data as T
 import qualified Tct.Core.Common.Pretty as PP
 
 -- | Standard answer type.
 data Answer
-  = Yes (Complexity, Complexity)
+  = Yes (T.Complexity, T.Complexity)
   | Unknown
   | No 
   deriving Show
 
 instance PP.Pretty Answer where
-  pretty (Yes (lb, ub)) = PP.text "YES" PP.<> PP.tupled [PP.pretty lb, PP.pretty ub]
-  pretty Unknown        = PP.text "MAYBE"
-  pretty No             = PP.text "NO"
+  pretty (Yes (lb, ub)) 
+    | lb /= T.Unknown || ub /= T.Unknown = PP.text "YES" PP.<> PP.tupled [PP.pretty lb, PP.pretty ub]
+  pretty No = PP.text "NO"
+  pretty _  = PP.text "MAYBE"
 
 -- | Returns the time upper bound as an answer.
-answering :: Return (ProofTree l) -> SomeAnswer
-answering = answer . returning (cert . certificate) (const Unknown)
-  where cert c = Yes (timeLB c, timeUB c)
+answering :: T.Return (T.ProofTree l) -> T.SomeAnswer
+answering = T.answer . T.returning (cert . T.certificate) (const Unknown)
+  where cert c = Yes (T.timeLB c, T.timeUB c)
 
