1409da88d6b6a79afb67fa549b1a72694c801fbc
mschaper <michael.schaper@uibk.ac.at>
2015-02-12 13:33:30 +0100

    again: SMT interface with new spawn command

--- src/Tct/Common/SMT.hs
+++ src/Tct/Common/SMT.hs
@@ -1,12 +1,22 @@
 module Tct.Common.SMT
   (
   module SMT
+  , minismt, minismt'
+  , yices, yices'
+  , z3, z3'
   ) where
 
 
-import SLogic.Smt as SMT 
+import           Control.Applicative        ((<$>))
+import qualified Data.ByteString            as BS
+import qualified Data.Map                   as M
 
-import Tct.Common.Ring
+import           SLogic.Smt                 as SMT hiding (minismt, minismt', yices, yices', z3, z3')
+
+import           Tct.Core.Common.Concurrent
+
+import           Tct.Common.Ring
+
 
 instance Additive SMT.IExpr where
   zero = SMT.zero
@@ -18,4 +28,27 @@ instance Multiplicative SMT.IExpr where
 
 instance AdditiveGroup SMT.IExpr where
   neg = SMT.neg
+
+
+smtSolver :: Cmd -> Args -> (t -> BS.ByteString) -> (String -> Result v) -> t -> IO (Result v)
+smtSolver cmd args formatter parser st =
+  either SMT.Error parser <$> spawn cmd args (`BS.hPutStr` formatter st)
+
+minismt' :: Args -> SolverState Expr -> IO (Result (M.Map Var Value))
+minismt' args = smtSolver "minismt" args minismtFormatter minismtParser
+
+minismt :: SolverState Expr -> IO (Result (M.Map Var Value))
+minismt = minismt' ["-m", "-v2", "-neg"]
+
+yices' :: Args -> SolverState Expr -> IO (Result (M.Map Var Value))
+yices' args = smtSolver "yices-smt2" args yicesFormatter yicesParser
+
+yices :: SolverState Expr -> IO (Result (M.Map Var Value))
+yices = yices' []
+
+z3' :: Args -> SolverState Expr -> IO (Result (M.Map Var Value))
+z3' args = smtSolver "z3" args z3Formatter z3Parser
+
+z3 :: SolverState Expr -> IO (Result (M.Map Var Value))
+z3 = z3' ["-smt2", "-in"]
 
