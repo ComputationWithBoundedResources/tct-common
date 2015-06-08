29323626f3e6f4799c7e3253426d562965090872
mschaper <michael.schaper@uibk.ac.at>
2015-06-08 09:54:48 +0200

    generalise SMT invokation
    The function smtSolve considers the runtime state, in particular solver
    and remaining time field, to decide which solver is invoked.

--- src/Tct/Common/SMT.hs
+++ src/Tct/Common/SMT.hs
@@ -4,6 +4,7 @@ module Tct.Common.SMT
   , minismt, minismt'
   , yices, yices'
   , z3, z3'
+  , smtSolve
   -- encoding
   , encodePoly
   ) where
@@ -11,11 +12,13 @@ module Tct.Common.SMT
 
 import           Control.Monad.Trans        (MonadIO, liftIO)
 import qualified Data.ByteString            as BS
+import           Data.List                  (nub)
 import qualified Data.Map                   as M
 
 import           SLogic.Smt                 as SMT hiding (minismt, minismt', yices, yices', z3, z3')
 
 import           Tct.Core.Common.Concurrent
+import qualified Tct.Core.Data              as T
 
 import qualified Tct.Common.Polynomial      as P
 import           Tct.Common.Ring
@@ -37,6 +40,19 @@ smtSolver :: MonadIO m => Cmd -> Args -> (t -> BS.ByteString) -> (String -> Resu
 smtSolver cmd args formatter parser st = do
   errM <- liftIO $ spawn cmd args (`BS.hPutStr` formatter st)
   return $ either SMT.Error parser errM
+
+smtSolve :: p -> SolverState Expr -> T.TctM (Result (M.Map Var Value))
+smtSolve p st = do
+  mso <- T.solver `fmap` T.askState
+  mto <- T.remainingTime `fmap` T.askStatus p
+  case mso of
+    Just (cmd,args)
+      | cmd == "minismt" -> minismt' (nub $ ["-m", "-v2", "-neg"] ++ args) mto  st
+      | cmd == "yices"   -> yices' args st
+      | cmd == "z3"      -> z3' (nub $ ["-smt2", "-in"] ++ args) mto st
+      | otherwise        -> defl mto
+    Nothing -> defl mto
+    where defl mto = minismt' ["-m", "-v2", "-neg"] mto st
 
 minismt' :: MonadIO m => Args -> Maybe Int -> SolverState Expr -> m (Result (M.Map Var Value))
 minismt' args mto = smtSolver "minismt" (args++to) minismtFormatter minismtParser
